921e09748d42b911f4833111a6a6e55f48520a4e
Duje Radman Livaja <dujeradmanlivaja@MacBook-Air-od-Duje.local>
2023-10-29 20:14:38 +0100

    final

---
+++ pages/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { useUser } from '@auth0/nextjs-auth0/client';
+import CompetitionsForm from '../pages/components/CompetitionForm';
+import Link from 'next/link';
+
+function Index() {
+  const { user, error, isLoading } = useUser();
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="p-8 bg-white rounded shadow-md text-center">
+        {isLoading && <div className="text-center">Loading...</div>}
+        {error && <div className="text-center text-red-600">{error.message}</div>}
+        {user ? (
+          <div>
+            <div className="text-center text-2xl font-bold mb-4">Welcome {user.name}!</div>
+            <Link href="/api/auth/logout" passHref={true} legacyBehavior={true}>
+              <a className="inline-block bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-600 transition duration-200 mb-4">
+                Logout
+              </a>
+            </Link>
+            <CompetitionsForm user={user} />
+          </div>
+        ) : (
+          <Link href="/api/auth/login" passHref={true} legacyBehavior={true}>
+            <a className="inline-block bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition duration-200">
+              Login
+            </a>
+          </Link>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Index;
+
